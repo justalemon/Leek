d2a0b621f84ea252f62b3daf5b8b628389a7223d
Hannele Ruiz <justlemoncl@gmail.com>
2023-01-09 23:22:19 -0300

    Added cog loading from command line

--- leek/__main__.py
+++ leek/__main__.py
@@ -1,8 +1,9 @@
+import importlib
 import logging
 import os
 import sys
 
-from discord import Intents
+from discord import Intents, Cog
 from dotenv import load_dotenv
 
 from .bot import LeekBot
@@ -20,6 +21,44 @@ def main():
         sys.exit(2)
 
     bot = LeekBot(intents=Intents.all())
+
+    cogs_to_load = os.environ.get("LEEK_COGS", "").split(",")
+
+    for name in cogs_to_load:
+        name = name.strip()
+
+        if name == "":
+            continue
+
+        split = name.split(":")
+
+        if len(split) != 2:
+            LOGGER.error(f"Cog name '{name}' is not in the right format")
+            continue
+
+        try:
+            imported = importlib.import_module(split[0])
+        except ModuleNotFoundError:
+            LOGGER.error(f"Unable to import '{name}' because it couldn't be found")
+            continue
+
+        if not hasattr(imported, split[1]):
+            LOGGER.error(f"Unable to load '{split[1]}' from '{split[1]} because the class does not exists")
+            continue
+
+        cog = getattr(imported, split[1])
+
+        if not issubclass(cog, Cog):
+            LOGGER.error(f"Class '{name}' does not inherits from a Cog")
+            continue
+
+        try:
+            bot.add_cog(cog(bot))
+        except BaseException as e:
+            LOGGER.error(f"Unable to start '{name}': {e}")
+        finally:
+            LOGGER.info(f"Added cog {name}")
+
     bot.run(os.environ["DISCORD_TOKEN"])
 
 

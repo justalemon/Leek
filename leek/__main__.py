cff5275e690e26ee0253b4ddccca9e318c7a4bf8
Hannele Ruiz <justlemoncl@gmail.com>
2023-01-09 21:55:13 -0300

    Added check to make sure that the token exists

--- leek/__main__.py
+++ leek/__main__.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 
 from dotenv import load_dotenv
 
@@ -12,6 +13,10 @@ def main():
     logging.basicConfig(level=logging.INFO)
 
     load_dotenv()
+
+    if "DISCORD_TOKEN" not in os.environ:
+        LOGGER.error("Discord Token is not set")
+        sys.exit(2)
 
     bot = LeekBot()
     bot.run(os.environ["DISCORD_TOKEN"])

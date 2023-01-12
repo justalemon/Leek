c813ae3f19b287ba54367fec433bb93770bd2bc9
Hannele Ruiz <justlemoncl@gmail.com>
2023-01-12 04:10:26 -0300

    Specify the cog name in the exception

--- leek/exception.py
+++ leek/exception.py
@@ -1,3 +1,6 @@
+from discord import Cog
+
+
 class LeekError(Exception):
     """
     The basic exception for all Leek exceptions.
@@ -9,4 +12,5 @@ class DatabaseRequiredError(LeekError):
     """
     An exception raised when the database pool connection is required but is not available.
     """
-    pass
+    def __init__(self, cog: Cog):
+        super().__init__(f"Cog {cog.qualified_name} requires a database connection but is not available")
